6d817eb717c5db78f5f430569ba0d24978654d37
Kaze Shindo <kaze.bknb@outlook.jp>
2021-01-17 18:52:37 +0900

    おうむ返し実装

--- main.py
+++ main.py
@@ -41,6 +41,12 @@ def callback():
 
 # 以降で ボット処理内容について記載 =========================================
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    text_sent_by_user = event.message.text
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=text_sent_by_user))
 
 # ====================================================================
 
